f32e6a71c649118756b165e5c7db68d85addf55c
Ian R <ian.n.rohan@gmail.com>
2025-10-21 10:15:08 -0500

    Modified User.java

--- src/main/java/edu/utsa/teamcodex/elibrary/model/User.java
+++ src/main/java/edu/utsa/teamcodex/elibrary/model/User.java
@@ -8,18 +8,19 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String username;
     private String password;
     private String email;
+    private String role;
 
     // Constructors
     public User() {}
 
-    public User(String username, String password, String email) {
+    public User(String username, String password, String email, String role) {
         this.username = username;
         this.password = password;
         this.email = email;
+        this.role = role;
     }
 
     // Getters & Setters
@@ -30,4 +31,6 @@ public class User {
     public void setPassword(String password) { this.password = password; }
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
 }
